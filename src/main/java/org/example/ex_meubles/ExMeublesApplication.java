c9072dbb3326c9f7b54071afd4234f74610a5047
fbanier <frbanier@gmail.com>
2025-06-20 14:31:10 +0200

    first commit

---
+++ src/main/java/org/example/ex_meubles/ExMeublesApplication.java
@@ -0,0 +1,13 @@
+package org.example.ex_meubles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMeublesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExMeublesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ex_meubles/ExMeublesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ex_meubles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExMeublesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
